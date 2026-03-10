38bec38c840ee8c450278d48afcaf1f967ecfb7a
Burak <ahmetcanburak54@gmail.com>
2026-03-10 21:32:16 +0300

    daily commit

---
+++ 2nd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+cout << "Please enter your first name and age\n";
+string first_name;
+int age = -1;
+cin >>first_name>>age;
+cout << "Hello, " << first_name << " (age " << age << ")\n";
+}
+
+
+
+
