b2d001269518e84d9a58a79a8cb0813e15ab3ad5
Burak <ahmetcanburak54@gmail.com>
2026-03-12 20:26:56 +0300

    lets start the day

---
+++ second.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+cout<< "Please enter your name and age\n";
+string first_name ="???";
+int age = -1;
+cin >> first_name>>age;
+cout << "Hello, " << first_name<< "(age" <<age<< ")\n";
+}
