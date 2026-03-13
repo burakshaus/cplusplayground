6d96b76683b2c20fe61983d05f4b63491d194305
Burak <ahmetcanburak54@gmail.com>
2026-03-13 22:05:34 +0300

    daily commit

---
+++ 5th.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+string first;
+string second;
+cout <<"Please enter your first name and second names\n";
+cin >> first >> second;
+string name = first +second;
+cout <<"Hello, "<<name<<'\n';
+}
