3dd60fbb51fd4095b712a903dff66aa74c867cba
Burak <ahmetcanburak54@gmail.com>
2026-03-16 20:04:50 +0300

    daily commit

--- minisensorreader.cpp
+++ minisensorreader.cpp
@@ -9,9 +9,11 @@ random_device rd;
 mt19937 generator(rd());
 uniform_int_distribution<int> distribution(0,1023);
 int random_number = distribution(generator);
-cout <<"random number" << random_number<< '\n';
+//cout <<"random number" << random_number<< '\n';
 uniform_real_distribution<double> dis(0.0, 5.0);
 double random_volt = dis(generator);
+double casted = static_cast<double>(random_number);
+double volt = casted/1023.0*5.0;
 cout << fixed << setprecision(4);
-cout <<"Sensor Voltage : " << random_volt << "V" << '\n';
-}
+cout << "the calculated value: " << volt <<"V" <<'\n';
+};
