829105b162d9f5e98df2dc20960f370869eed355
Burak <ahmetcanburak54@gmail.com>
2026-03-19 15:44:43 +0300

    daily commit

---
+++ drill1.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+#include <string>
+using namespace std; 
+int main (){
+string first_name; 
+cout << "could you please type your firstname" << '\n';
+cin >> firstname;
+cout << "Dear, " << first_name << '\n';
+
+}
