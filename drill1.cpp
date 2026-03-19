cb71cef6f4c0ae1459637163f1c83a177b32cb4c
Burak <ahmetcanburak54@gmail.com>
2026-03-19 17:34:11 +0300

    daily commit

--- drill1.cpp
+++ drill1.cpp
@@ -4,30 +4,27 @@ using namespace std;
 int main (){
 string first_name; 
 cout << "could you please type your firstname" << '\n';
-cin >> firstname;
+cin >> first_name;
 cout << "Dear, " << first_name << ". How are you i am fine thanks, i miss you my friend "<< '\n';  
 string friend_name; 
 cout << "enter your fiend's name" << '\n';
 cin >> friend_name; 
 int age ;
-cout << "Have you seen " << friends_name << "lately?" << '\n';
+cout << "Have you seen " << friend_name << "lately?" << '\n';
 cout << "enter your age"; 
 cin >> age ; 
 cout << "I hear you just had a birthday and you are " << ++age << "years old" << '\n';
 if(age <= 0 && age >= 110){
-simple_error("you're kissing !");
+cout << "you're kidding !" << '\n';
 }
 if(age < 12){
-cout "You will be " << age+1 << "next year" << '\n'; 
+cout << "You will be " << age+1 << "next year" << '\n'; 
 }else if(age == 17){
 cout << "Next year you will be able to vote" << '\n';
 }
 else if (age>70){
-cout cout "Are you retired?" << '\n';
+cout << "Are you retired?" << '\n';
 }
-cout " Yours sincerely,\n\n Ahmet Burak Can"; 
-}
-int simple_error(string message){
-cout << message << '\n'; 
-
+cout << " Yours sincerely,\n\n Ahmet Burak Can" << '\n'; 
+return 1;
 }
