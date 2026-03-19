0fb9905e61d96f96e75f8658b170715832e8570f
Burak <ahmetcanburak54@gmail.com>
2026-03-19 16:12:25 +0300

    drill1 is done

--- drill1.cpp
+++ drill1.cpp
@@ -5,6 +5,29 @@ int main (){
 string first_name; 
 cout << "could you please type your firstname" << '\n';
 cin >> firstname;
-cout << "Dear, " << first_name << '\n';
+cout << "Dear, " << first_name << ". How are you i am fine thanks, i miss you my friend "<< '\n';  
+string friend_name; 
+cout << "enter your fiend's name" << '\n';
+cin >> friend_name; 
+int age ;
+cout << "Have you seen " << friends_name << "lately?" << '\n';
+cout << "enter your age"; 
+cin >> age ; 
+cout << "I hear you just had a birthday and you are " << ++age << "years old" << '\n';
+if(age <= 0 && age >= 110){
+simple_error("you're kissing !");
+}
+if(age < 12){
+cout "You will be " << age+1 << "next year" << '\n'; 
+}else if(age == 17){
+cout << "Next year you will be able to vote" << '\n';
+}
+else if (age>70){
+cout cout "Are you retired?" << '\n';
+}
+cout " Yours sincerely,\n\n Ahmet Burak Can"; 
+}
+int simple_error(string message){
+cout << message << '\n'; 
 
 }
