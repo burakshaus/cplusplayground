f3e32c095bcd65fb3527f3069a0fa5fdb47b6480
Burak <ahmetcanburak54@gmail.com>
2026-03-23 20:41:03 +0300

    daily commit

--- seppednumbers.cpp
+++ seppednumbers.cpp
@@ -5,9 +5,33 @@
 
 using namespace std;
 int main(){
-string str;
+string input
 cout<<"type your number"<<'\n';
-cin>>str;
-str=str.split(' ');
-string[] array[9]=["twenty","thiry","fourty","fifty","sixty","seventy","eigthy","ninety"];
+cin>>input;
+std::cout<<"please enter a spelled-out number (e.g., 'zero','two'):"
+while(std::cin>>input){
+if(input==zero){
+cout<<0<<'\n';
+}else if(input=="one"){
+cout<<1<<'\n';
+}else if(input =="two"){
+cout<<2<<'\n';
+}else if(input=="three"){
+cout<<3<<'\n';
+}else if(input=="four"){
+cout<<4<<'\n';
+}else if(input=="five"){
+cout<<5<<'\n';
+}
+}else if(input=="six"){
+cout<<6<<'\n';
+}else if(input=="seven"){
+cout<<7<<'\n'
+}elseif(input=="eigth"){
+cout<<8<<'\n';
+}else if(input==nine"){
+cout<<9<<'\n';
+}else {
+cout<<"Not a number i know\n";
+}
 }
