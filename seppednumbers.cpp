43f15c6fa92ea62518eaa76b61a60739d308c916
Burak <ahmetcanburak54@gmail.com>
2026-03-23 20:21:14 +0300

    daily commit

---
+++ seppednumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <ranges>
+#include <vector>
+
+using namespace std;
+int main(){
+string str;
+cout<<"type your number"<<'\n';
+cin>>str;
+str=str.split(' ');
+string[] array[9]=["twenty","thiry","fourty","fifty","sixty","seventy","eigthy","ninety"];
+}
