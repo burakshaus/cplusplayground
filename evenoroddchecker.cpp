fdcebe0d93fa825b8b43a045267f4c0bf1190387
Burak <ahmetcanburak54@gmail.com>
2026-03-23 15:52:16 +0300

    daily commit

---
+++ evenoroddchecker.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"Please ebter a number"<<'\n';
+int num;
+cin>>num;
+if(num%2 == 0){
+cout<<num<<" is even\n";
+}else {
+cout<<num<<"is odd\n";
+}
+}
