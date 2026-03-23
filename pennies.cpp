007c9689c6190e7b0cf52815634785612c531fd8
Burak <ahmetcanburak54@gmail.com>
2026-03-23 21:32:32 +0300

    daily commit

---
+++ pennies.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+cout<<"plese enter what you have (e.g., 1 pennies, 1 nickels,14 dimes, vice versa):\n";
+string input;
+while(cin>>input){
+
+}
+}
