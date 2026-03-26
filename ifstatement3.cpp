65dae6a4e369ed6fc2f2e26a986bb1803593dac8
Burak <ahmetcanburak54@gmail.com>
2026-03-26 15:14:54 +0300

    daily commit

--- ifstatement3.cpp
+++ ifstatement3.cpp
@@ -10,7 +10,21 @@ cout<< "Please enter a length followed by a unit (c or i):\n";
 cin >> length >> unit;
 if (unit == 'i'){
 cout << length << "in == " << length*cm_per_inch << "cm\n";
-}else{
+}else if(unit=='c'){
 cout << length << "cm == " << length/cm_per_inch << "in\n";
+}else{
+cout <<"Sorry i dont know a unit called "<<"'"<<unit<<"'\n";
+}
+switch(unit){
+	case'i':
+		cout <<length<<"in=="<<length*cm_per_inch<<"cm\n";
+		break;
+	case 'c':
+		cout << length<<"cm== "<<length/cm_per_inch<<" in\n";
+		break;
+default:
+		cout<<"Sorry, I don't lknow a unit called '"<<unit<<"'\n";
+		break;
+
 }
 }
