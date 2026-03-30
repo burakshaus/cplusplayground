f6c8decf333df9780d2de7541e66ab9ef4743bbe
Burak <ahmetcanburak54@gmail.com>
2026-03-30 13:59:49 +0300

    daily commit

---
+++ twoints.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+int main(){
+	std::vector<int> array;
+	int in1,in2;
+	while(std::cin>>in1>>in2){
+	if(in1=='|'||in2=='|'){
+	break;
+	}else{
+
+	std::cout<<in1<<",  "<<in2<<",  ";
+	}
+	}
+
+
+}
