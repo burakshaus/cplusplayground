2a0276f3e2e503938c08792fc3f6c86a76b18213
Burak <ahmetcanburak54@gmail.com>
2026-03-31 15:30:15 +0300

    daily commit

--- unitperdouble.cpp
+++ unitperdouble.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <vector>
 int main(){
-	std::cout<<"Please type foalingpoins each followed by enter"<<std::endl;
+	std::cout<<"Please type foaling points each followed by enter"<<std::endl;
+	std::vector<double> array;
 	double input;
+	double sum,smallest,largest;
 	std::string unit;
 	while(std::cin>>input){
+		array.push_back(input);
+		if(input<smallest){
+		smallest = input;
+		}
+		if(input>largest){
+		largest = input;
+		}
 		std::cout<<"Please enter the unit (e.g. cm, in,m,ft) "<<std::endl;
 		std::string unit;
 		std::cin>>unit;
+		if(unit=="m"){
+			sum+=input;
+		}
+		std::cout<<"Now it's turn for ploatint point"<<std::endl;
 	}
+	std::cout<<"Smallest: "<<smallest<<" Largest: "<<largest<<" number of values:"<<array.size()<<std::endl;
 
 }
