1fe72784a564e9a361498c0cbb737309a8d5b2cd
Burak <ahmetcanburak54@gmail.com>
2026-04-01 21:08:32 +0300

    daily commit

--- three.one.cpp
+++ three.one.cpp
@@ -3,12 +3,15 @@
 #include <vector>
 int main(){
 	std::string input;
+	int a = 0;
 	std::vector<std::string> myvector;
 	std::cout<<"Please type the string you wanted (cnrtl + d is quit)"<<std::endl;
 	while(std::cin>>input){
 		myvector.push_back(input);
-		for(int i=0;i<myvector.size(); ++i){
-			std::cout<<"Position: "<<i<<", char: "<<myvector[i]<<". "<<std::endl;
+		for(int i=0;i<myvector.size(); ++i){	std::cout<<a<<std::endl;
+			if(i==0||a!=0){a+=1;
+				
+			}
 		}
 		myvector.clear();
 		if(myvector.empty()){
