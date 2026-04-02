0c6131ffbb07bd96b2a1c64f48b448c7f03806a4
Burak <ahmetcanburak54@gmail.com>
2026-04-02 19:12:00 +0300

    daily commit

--- test3.cpp
+++ test3.cpp
@@ -2,14 +2,10 @@
 #include <string>
 
 int main() {
-    std::string input;
-    
-    std::cout << "Enter a string: ";
-    // Use getline to include spaces, or cin >> input for a single word
-    std::getline(std::cin, input);
+	std::string input;
+std::getline(std::cin, input);
 
     for (char c : input) {
-        // Casting 'c' to (int) reveals its underlying numeric value
         std::cout << c << " " << static_cast<int>(c) << std::endl;
     }
 
