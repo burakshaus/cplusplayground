15e93e6cb60c23cd009487ad3f48603e287d9316
Burak <ahmetcanburak54@gmail.com>
2026-04-13 19:52:29 +0300

    daily commit

---
+++ differentmedian.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+vector<int> myarray = {3,7,6,9,6,2,3,1,2,6,9};
+int size = myarray.size();
+int median = size/2;
+cout <<median<<"\t\n";
+}
