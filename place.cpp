560a65adf4d592ed35c935f26cba654c5aaee23b
Burak <ahmetcanburak54@gmail.com>
2026-05-28 21:45:23 +0300

    daily commit

--- place.cpp
+++ place.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
-int main (){
-cout << "sqares of 1 to 9 \n";
-for(int i = 1; i<10; i++){
-cout<<i<< " \t"<< i*i;
 
-}
-
+int main () {
+    cout << "Squares of 1 to 9 \n";
+    
+    for(int i = 1; i < 10; i++) {
+        cout << i << "\t ";
+        cout << i * i << "\n";
+    }
+    
+    return 0;
 }
