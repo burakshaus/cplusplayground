a89d7bb927e805345188d492b53a08fe1cc65ed7
Burak <ahmetcanburak54@gmail.com>
2026-05-28 21:59:17 +0300

    daily commit

---
+++ squares.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Squares of 1 to 10\n";
+    for(int i = 1; i <= 10; i++) {
+        cout << i << "\t" << i * i << "\n";
+    }
+    return 0;
+}
