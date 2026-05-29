1ecf9ec89e4abf35b221b88940c3b01ff2c0dad1
Burak <ahmetcanburak54@gmail.com>
2026-05-29 12:40:52 +0300

    daily commit

---
+++ input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+int x,y,sum;
+cout <<"enter an int";
+cin>>x;
+cout<<"enter another int";
+cin>>y;
+sum =x+y;
+cout<<"sum : "<<sum<<std::endl;
+
+}
