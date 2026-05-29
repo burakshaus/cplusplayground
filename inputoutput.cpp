1abc48f80b642e3c2e10e86ab8d234594527eb56
Burak <ahmetcanburak54@gmail.com>
2026-05-29 17:49:29 +0300

    daily commit

--- inputoutput.cpp
+++ inputoutput.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     char fname[20]; 
     char sname [20];
+    char lname [20];
     
     cout << "Enter your first name: " << std::endl;
     cin.getline(fname, 20); 
@@ -12,5 +13,8 @@ int main() {
     cout << "enter your second name:"<<std::endl;
     cin.getline(sname,20);
     cout<<"Your second name is : "<<sname<<std::endl;
+    cout <<"enter your last name"<<std::endl;
+    cin.getline(lname,20);
+    cout<<"your lastname is : "<<lname<<std::endl;
     return 0;
 }
